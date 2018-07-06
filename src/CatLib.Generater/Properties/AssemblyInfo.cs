ca232e313196739d1af9dd75ae3e8e660112c880
喵喵大人 <support@catlib.io>
2018-07-06 15:14:11 +0800

    project created

---
+++ src/CatLib.Generater/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * This file is part of the CatLib package.
+ *
+ * (c) Yu Bin <support@catlib.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Document: http://catlib.io/
+ */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CatLib.Generater")]
+[assembly: AssemblyDescription("CatLib Code Generater")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CatLib")]
+[assembly: AssemblyProduct("CatLib.Generater")]
+[assembly: AssemblyCopyright("Copyright ©  2017-2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2539df46-f90c-4707-9606-a010b03d4cda")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ src/CatLib.Generater/UnityMenuEntry.cs
@@ -0,0 +1,13 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+
+//public class UnityMenuEntry
+//{
+//	[MenuItem("CatLib/CodeGenerater/FacadeFile")]
+//	public static void CatLib_CodeGenerater_FacadeFile()
+//	{
+//		CatLib_CodeGenerater_APIs.GenerateFacadeFiles ( Application.dataPath + "/../FacadeFiles/", "GenerateFacadeAttribute", "", "GenerateFacadeAPIAttribute" ,Application.dataPath + "/../FacadeFiles/template.txt");
+//	}
+//}
