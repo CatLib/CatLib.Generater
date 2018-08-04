bf1a1cb5f6a858b3643e52245ed42bd983dc6561
喵喵大人 <support@catlib.io>
2018-08-04 18:18:08 +0800

    update

--- src/CatLib.Generater.Editor.Tests/Policy/MemberStaticWrapPolicyTests.cs
+++ src/CatLib.Generater.Editor.Tests/Policy/MemberStaticWrapPolicyTests.cs
@@ -11,59 +11,16 @@
 
 using CatLib.Generater.Editor.Context;
 using CatLib.Generater.Editor.Policy;
+using CatLib.Generater.Editor.Policy.StaticWrap;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Reflection.Emit;
-using CatLib.Generater.Editor.Policy.StaticWrap;
 
 namespace CatLib.Generater.Editor.Tests.Policy
 {
     [TestClass]
     public class MemberStaticWrapPolicyTests
     {
-        public interface ITestInterfaceParent
-        {
-            int Hello();
-        }
-
-        public const string ddd = "123";
-
-        public interface ITestInterface<T> : ITestInterfaceParent, IDisposable
-        {
-            void Say();
-
-            event Action<int> TestEvent;
-
-            T Hello();
-
-            List<string> dddp();
-
-            int add_TestEvent();
-
-            void TestVoid(int a, out Dictionary<string, int> b, ref int c, params int[] d);
-
-            void TestOptional(int a = 100, float b = 12.2f, string c = ddd, string dd = null);
-
-            int TestAttribute { get; set; }
-        }
-
-        private event Action bb;
-
-
-        [TestMethod]
-        public void TestMethodsInterfacePolicy()
-        {
-            var policy = new MemberStaticWrapPolicy();
-            var context = new FacadeContext(null, typeof(ITestInterface<int>));
-            var classPolicy = new ClassCreatePolicy();
-            classPolicy.Factory(context);
-            policy.Factory(context);
-
-            Console.WriteLine(Util.Generate(context.CompileUnit));
-            //Assert.AreEqual(3, context.Class.Members.Count);
-        }
-
         #region 测试正常的方法
 
         public interface ITestNormalFunctionWrap
